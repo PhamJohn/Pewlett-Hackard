544c2140f32f18e498117ea1e77b33c4cb5adda3
codepast12 <62220303+codepast12@users.noreply.github.com>
2021-08-16 13:48:54 -0700

    Classwork + Challenge

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,53 @@
+--Module 7 Challenge - Deliverable 1
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Validate table retirement_titles
+SELECT * FROM retirement_titles;
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Validate table unique_titles
+SELECT * FROM unique_titles;
+
+SELECT COUNT(ut.emp_no),ut.title
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+--Module 7 Challenge - Deliverable 2
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+-- Validate table retirement_titles
+SELECT * FROM mentorship_eligibilty
